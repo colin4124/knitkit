3adceb53f97d9fb801c7c29316b4a24accf91c49
Leway Colin <colin4124@gmail.com>
2021-11-24 01:14:30 +0800

    CatGroup WIP

--- src/stmts/Cat.scala
+++ src/stmts/Cat.scala
@@ -1,5 +1,7 @@
 package knitkit
 
+import math.pow
+
 import ir._
 import internal.Builder.{pushOp, error}
 
@@ -20,7 +22,7 @@ object CatGroup {
   def apply[T <: Bits](r: Seq[T], group_num: Int = 0, prefix: String = "CAT"): Bits = r match {
     case b: Seq[Bits] =>
       require(group_num > 1, s"${group_num} must > 1")
-      group_cat(r, group_num, prefix, r.size - 1, 0, group_num)
+      group_cat(r, group_num, prefix, r.size - 1, 0, 0)
     case _ =>
       error("Not support")
   }
@@ -32,8 +34,10 @@ object CatGroup {
       val cur_cat = (idx_list zip grouped) map { case (idx, sub) =>
         // println(idx)
         // println(sub)
-        val child_msb = msb - num * idx
-        val child_lsb = child_msb - level + 1
+        val gap = pow(num, level+1).toInt
+        val child_msb = msb - gap * idx
+        val pending_lsb = child_msb - gap + 1
+        val child_lsb = if (pending_lsb < 0) 0 else pending_lsb
         // println(s"${child_msb}:${child_lsb}")
         if (sub.size == 1) {
           sub(0)
@@ -42,8 +46,12 @@ object CatGroup {
           WireInit(child).suggestName(s"${prefix}_${child_msb}_${child_lsb}")
         }
       }
-      println(s"${msb}:${lsb}")
-      WireInit(group_cat(cur_cat, num, prefix, msb, lsb, level+num)).suggestName(s"${prefix}_${msb}_${lsb}")
+      val result = group_cat(cur_cat, num, prefix, msb, lsb, level+1)
+      if (level < 2) {
+        result
+      } else {
+        WireInit(result).suggestName(s"${prefix}_${msb}_${lsb}")
+      }
     } else if (r.size == 1) {
       r(0)
     } else {

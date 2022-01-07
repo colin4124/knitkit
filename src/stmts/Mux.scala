912a541d8eb5669412a5028ef25289cb5cad555c
Leway Colin <colin4124@gmail.com>
2022-01-07 11:48:41 +0800

    Support Mux for Vec

--- src/stmts/Mux.scala
+++ src/stmts/Mux.scala
@@ -10,24 +10,42 @@ object Mux {
     dest
   }
 
-  def apply(cond: Bits, con: Data, alt: Data): Bits = {
-    (con, alt) match {
-	    case (c: Bits, a: Bits) =>
-        cond.width match {
-          case IntWidth(x) =>
-            require(x == 1, s"Conditional bit width must be 1, not $x")
-          case UnknownWidth =>
-            Builder.error(s"Cond width must be 1")
-        }
-        requireIsHardware(cond, "mux condition")
-        requireIsHardware(c   , "mux true value")
-        requireIsHardware(a   , "mux false value")
-        val elt = if (cond.width == (cond.width max a.width)) c else a
-        val dest = elt.cloneType
-        pushMux(dest, cond.ref, c.ref, a.ref)
-      case _ =>
-        Builder.error(s"Mux's params type Bits only")
+  def check(cond: Bits, con: Data, alt: Data): Unit = {
+    cond.width match {
+      case IntWidth(x) =>
+        require(x == 1, s"Conditional bit width must be 1, not $x")
+      case UnknownWidth =>
+        Builder.error(s"Cond width must be 1")
     }
+    requireIsHardware(cond, "mux condition")
+    requireIsHardware(con , "mux true value")
+    requireIsHardware(alt , "mux false value")
+  }
+
+  def apply(cond: Bits, con: Bits, alt: Bits): Bits = {
+    check(cond, con, alt)
+    val elt = if (cond.width == (cond.width max alt.width)) con else alt
+    val dest = elt.cloneType
+    pushMux(dest, cond.ref, con.ref, alt.ref)
+  }
+
+  def apply(cond: Bits, con: Vec, alt: Vec): Vec = {
+    check(cond, con, alt)
+    Vec((con.elements zip alt.elements) map { case (c, a) =>
+      apply(cond, c, a)
+    })
+  }
+
+  def apply(cond: Bits, con: Data, alt: Data): Data = {
+    check(cond, con, alt)
+    (con, alt) match {
+	   case (c: Bits, a: Bits) =>
+       apply(cond, c, a)
+	   case (c: Vec, a: Vec) =>
+       apply(cond, c, a)
+     case _ =>
+       Builder.error(s"Mux's params type Bits only")
+   }
   }
 }
 

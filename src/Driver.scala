8810c5539165fa693d0024504e370e2a8f96e3f2
Leway Colin <colin4124@gmail.com>
2022-04-17 21:02:04 +0800

    Generated one file or not depends on option

--- src/Driver.scala
+++ src/Driver.scala
@@ -10,14 +10,27 @@ object Driver {
     val (circuit, _) = Builder.build(Module(dut()))
     Emitter.emit(circuit) map { case (_, v) => v } reduce { _ + _ }
   }
-  def execute(dut: () => RawModule, dump_dir: String): Unit = {
+  def execute(dut: () => RawModule, dump_dir: String, one_file: Boolean = true): Unit = {
     val (circuit, _) = Builder.build(Module(dut()))
 
-    val contents = Emitter.emit(circuit) map { case (_, v) => v } reduce { _ + _ }
-    Files.createDirectories(Paths.get(dump_dir));
-    val file = new File(s"${dump_dir}/${circuit.name}.v")
-    val w = new FileWriter(file)
-    w.write(contents)
-    w.close()
+    val result = Emitter.emit(circuit)
+
+    if (one_file) {
+      val contents = result map { case (_, v) => v } reduce { _ + _ }
+      Files.createDirectories(Paths.get(dump_dir));
+      val file = new File(s"${dump_dir}/${circuit.name}.v")
+      val w = new FileWriter(file)
+      w.write(contents)
+      w.close()
+
+    } else {
+      result foreach { case (m_name, v) =>
+        Files.createDirectories(Paths.get(dump_dir));
+        val file = new File(s"${dump_dir}/${m_name}.v")
+        val w = new FileWriter(file)
+        w.write(v)
+        w.close()
+      }
+    }
   }
 }

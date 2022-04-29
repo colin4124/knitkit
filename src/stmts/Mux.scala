1c25bd074cbb443934c286751bf4140f45ff1d27
Leway Colin <colin4124@gmail.com>
2022-04-30 00:44:23 +0800

    Refactor Mux param's type

--- src/stmts/Mux.scala
+++ src/stmts/Mux.scala
@@ -26,76 +26,50 @@ object Mux {
     requireIsHardware(alt , "mux false value")
   }
 
-  def apply[T <: Bits](cond: Bits, con: T, alt: T): T = {
-    check(cond, con, alt)
-    val elt = if (cond.width == (cond.width max alt.width)) con else alt
-    val dest = elt.cloneType
-    pushMux(dest, cond.ref, con.ref, alt.ref)
-  }
-
-  def apply(cond: Bits, con: Vec, alt: Vec): Vec = {
-    check(cond, con, alt)
-    Vec((con.elements zip alt.elements) map { case (c, a) =>
-      apply(cond, c, a)
-    })
-  }
-
-  def apply(cond: Bits, con: Aggregate, alt: Aggregate): Aggregate = {
-    check(cond, con, alt)
-    Agg(con.eles map { case (name, ele) =>
-      name -> apply(cond, ele, alt(name))
-    })
-  }
-
-  def apply(cond: Data, con: Data, alt: Data): Data = {
-    apply(cond.asBits, con, alt)
-  }
-
-  def apply(cond: Bits, con: Data, alt: Data): Data = {
+  def apply[T <: Data](cond: Data, con: T, alt: T): T = {
     check(cond, con, alt)
     (con, alt) match {
-	   case (c: Bits, a: Bits) =>
-       apply(cond, c, a)
-	   case (c: Vec, a: Vec) =>
-       apply(cond, c, a)
-	   case (c: Aggregate, a: Aggregate) =>
-       apply(cond, c, a)
-     case _ =>
-       Builder.error(s"Mux's Must both are Bits, Vec, Or Agg")
-   }
+      case (c: Bits, a: Bits) =>
+        val elt = if (c.width == (c.width max a.width)) c else a
+        val dest = elt.cloneType
+        pushMux(dest, cond.ref, c.ref, a.ref).asInstanceOf[T]
+      case (c: Vec, a: Vec) =>
+        Vec((c.elements zip a.elements) map { case (c, a) =>
+              apply(cond, c, a)
+            }).asInstanceOf[T]
+      case (c: Aggregate, a: Aggregate) =>
+        Agg(c.eles map { case (name, ele) =>
+          name -> apply(cond, ele, a(name))
+        }).asInstanceOf[T]
+      case _ =>
+        Builder.error(s"Mux's Must both are Bits, Vec, Or Agg")
+
+    }
   }
 }
 
 object MuxCase {
-  def apply(default: Data, mapping: Seq[(Bits, Data)]): Data = {
+  def apply[T <: Data](default: T, mapping: Seq[(Data, T)]): T = {
     var res = default
     for ((t, v) <- mapping.reverse){
       res = Mux(t, v, res)
     }
     res
   }
+}
 
-  def apply(default: Vec, mapping: Seq[(Bits, Vec)]): Vec = {
-    var res = default
-    for ((t, v) <- mapping.reverse){
-      res = Mux(t, v, res)
-    }
-    res
-  }
+object PriorityMux {
+  def apply[T <: Data](in: Seq[(Bits, T)]): T = priorityMux(in)
+  def apply[T <: Data](sel: Seq[Bits], in: Seq[T]): T = apply(sel.zip(in))
+  def apply[T <: Data](sel: Bits, in: Seq[T]): T = apply((0 until in.size).map(sel(_)), in)
 
-  def apply(default: Aggregate, mapping: Seq[(Bits, Aggregate)]): Aggregate = {
-    var res = default
-    for ((t, v) <- mapping.reverse){
-      res = Mux(t, v, res)
+  def priorityMux[T <: Data](
+    in: Seq[(Bits, T)]
+  ): T = {
+    if (in.size == 1) {
+      in.head._2
+    } else {
+      Mux(in.head._1, in.head._2, priorityMux(in.tail))
     }
-    res
-  }
-
-  def apply[T <: Bits](default: T, mapping: Seq[(Bits, T)]): T = {
-    var res = default
-    for ((t, v) <- mapping.reverse){
-      res = Mux(t, v, res)
-    }
-    res
   }
 }
